f29b77f651c047dda435cc0b72985a81e1738719
deuwin <wcully@gmail.com>
2023-06-01 20:05:31 +0100

    zsh: Allow for local rc extensions/overrides

--- zsh/rc/10-misc.zsh
+++ zsh/rc/10-misc.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 ###
 # Miscellaneous
 #
@@ -6,10 +8,3 @@
 autoload -Uz url-quote-magic bracketed-paste-magic
 zle -N self-insert url-quote-magic
 zle -N bracketed-paste bracketed-paste-magic
-
-# Clean up helpers and log any commands not found
-if ((${#command_missing} > 0)); then
-    _impure_warning "Command(s) not found: ${(j:, :)command_missing}"
-fi
-unset command_missing
-unfunction is_command is_function
---
+++ zsh/rc/99-cleanup.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+###
+# Clean up
+#
+
+# Clean up helpers and log any commands not found
+if ((${#command_missing} > 0)); then
+    _impure_warning "Command(s) not found: ${(j:, :)command_missing}"
+fi
+unset command_missing
+unfunction is_command is_function
