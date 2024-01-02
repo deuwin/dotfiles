56d99da65fdfd740da47b5ce9617a011f4d713a5
deuwin <wcully@gmail.com>
2024-01-02 00:39:30 +0000

    zsh: Add fzf-git override

---
+++ zsh/third_party/overrides/fzf-git.zsh
@@ -0,0 +1,31 @@
+# tweak fzf-git
+#   - remove browser shortcut
+#   - use delta to render diffs if available
+if is_function _fzf_git_files; then
+    _fzf_git_files() {
+        local gdiff="git diff --no-ext-diff --color=always -- {-1} | "
+        () {
+            if command -v delta > /dev/null; then
+                gdiff+="delta"
+            else
+                gdiff+="sed 1,4d"
+            fi
+        }
+
+        _fzf_git_check || return
+        (
+            git -c color.status=always status --short
+            git ls-files --others \
+                | grep --invert-match --line-regexp --fixed-strings \
+                    --file=<(
+                        git status --short | grep '^[^?]' | cut -c4-; echo :) \
+                | sed 's/^/   /'
+        ) | \
+            _fzf_git_fzf --multi --ansi --nth 2..,.. \
+                --border-label "📁 Files" \
+                --header "ALT-E (open in editor)" \
+                --bind "alt-e:execute:${EDITOR:-vim} {-1} > /dev/tty" \
+                --preview "$gdiff; $_fzf_git_cat {-1}" "$@" \
+                | cut -c4- | sed 's/.* -> //'
+    }
+fi
