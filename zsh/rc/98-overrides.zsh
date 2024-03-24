04ebb7442a5536a4494001c5aae5faf58a1de632
deuwin <wcully@gmail.com>
2024-03-24 17:12:59 +0000

    zsh: Change overrides from anonymous function

--- zsh/rc/98-overrides.zsh
+++ zsh/rc/98-overrides.zsh
@@ -3,17 +3,17 @@
 # apply changes to functions, options, or any other setting previously
 # established
 #
-() {
-    local override
-    setopt localoptions nullglob
+local override
+setopt localoptions nullglob
 
-    # overrides for third party scripts 
-    for override in $ZDOTDIR/third_party/overrides/*.zsh; do
-        source $override
-    done
+# overrides for third party scripts 
+for override in $ZDOTDIR/third_party/overrides/*.zsh; do
+    source $override
+done
 
-    # overrides and extensions specific to this machine
-    for override in $ZDOTDIR/local/*.zsh; do
-        source $override
-    done
-}
+# overrides and extensions specific to this machine
+for override in $ZDOTDIR/local/*.zsh; do
+    source $override
+done
+
+unset override
