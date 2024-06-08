198138e7540fac69fd575a47fea63a3866d6baa9
deuwin <wcully@gmail.com>
2024-06-08 15:51:21 +0100

    zsh/third_party: Update fzf-git override

--- zsh/third_party/overrides/fzf-git.zsh
+++ zsh/third_party/overrides/fzf-git.zsh
@@ -3,29 +3,30 @@
 #   - use delta to render diffs if available
 if is_function _fzf_git_files; then
     _fzf_git_files() {
-        local gdiff="git diff --no-ext-diff --color=always -- {-1} | "
-        () {
-            if command -v delta > /dev/null; then
-                gdiff+="delta"
-            else
-                gdiff+="sed 1,4d"
-            fi
-        }
-
         _fzf_git_check || return
+        local root query
+        root=$(git rev-parse --show-toplevel)
+        [[ $root != "$PWD" ]] && query='!../ '
+
+        local gdiff="git diff --no-ext-diff --color=always -- {-1} | "
+        if command -v delta > /dev/null; then
+            gdiff+="delta"
+        else
+            gdiff+="sed 1,4d"
+        fi
+
         (
-            git -c color.status=always status --short
-            git ls-files --others \
-                | grep --invert-match --line-regexp --fixed-strings \
-                    --file=<(
-                        git status --short | grep '^[^?]' | cut -c4-; echo :) \
+            git -c color.status=$(__fzf_git_color) status --short --no-branch
+            git ls-files "$root" \
+                | grep -vxFf <(git status -s | grep '^[^?]' | cut -c4-; echo :) \
                 | sed 's/^/   /'
-        ) | \
-            _fzf_git_fzf --multi --ansi --nth 2..,.. \
-                --border-label "📁 Files" \
-                --header "ALT-E (open in editor)"$'\n\n' \
+        ) \
+            | _fzf_git_fzf -m --ansi --nth 2..,.. \
+                --border-label '📁 Files' \
+                --header $'ALT-E (open in editor)\n\n' \
                 --bind "alt-e:execute:${EDITOR:-vim} {-1} > /dev/tty" \
-                --preview "$gdiff; $_fzf_git_cat {-1}" "$@" \
+                --query "$query" \
+                --preview "$gdiff; $(__fzf_git_cat) {-1}" "$@" \
                 | cut -c4- | sed 's/.* -> //'
     }
 fi
