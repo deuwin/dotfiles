fba2bdaeb3458f678c07497e647cfe9cf6c02501
deuwin <wcully@gmail.com>
2024-07-20 13:30:40 +0100

    zsh: Update completion rules

--- zsh/rc/06-completion.zsh
+++ zsh/rc/06-completion.zsh
@@ -5,11 +5,11 @@
 # zsh-autocomplete
 # https://github.com/marlonrichert/zsh-autocomplete
 source $ZDOTDIR/third_party/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-## tab inserts common substring
-# all Tab widgets
+# tab inserts common substring
 zstyle ":autocomplete:*complete*:*" insert-unambiguous yes
-# all history widgets
-zstyle ":autocomplete:*history*:*" insert-unambiguous yes
+# match lower to uppercase, `-` to `_`,  and only insert longest matching
+# prefix without matching suffix
+zstyle ":completion:*" matcher-list "m:{[:lower:]-}={[:upper:]_}" "r:|?=**"
 # limit completions and history search to a third of the screen
 zstyle -e ":autocomplete:*" list-lines "reply=( $((LINES / 3)) )"
 # don't show completions that begin with two or more dots
